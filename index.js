8d7b5534649eeb8c3377b14d0c12ab6c02515c9c
s3ng0 <112876590+s3ng0@users.noreply.github.com>
2024-08-30 19:12:26 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Coucou la salière ❤️","Less Talk More Salt","Adorateur du Sel"];
 
 
 let currentIndex = 0;
